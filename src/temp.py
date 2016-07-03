dd5bb198ebf2cf385c3a46fb2b306ae267f0e024
junya <junya0220yamashita@gmail.com>
2016-07-03 18:29:37 +0530

    add a python code

---
+++ src/temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import serial
+import time
+
+print("test")
+
+# ser = serial.Serial('/dev/ttyACM0', 9600)
+# print(ser.portstr)
+# time.sleep(3)
+# while True:
+#     time.sleep(1)
+#     data = ser.readline()
+#     print(time.ctime() + ', ' + str(data))
